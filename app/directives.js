76fb7a9821bed468e26b0fc7bf82badc371e8269
adeopura3 <ashish.deopura@gmail.com>
2016-02-16 17:39:34 -0500

    Remove unneeded directives
    Removed aircraftDetails directive
    Added html to the parent to show html

--- app/directives.js
+++ app/directives.js
@@ -1,15 +1,1 @@
 // Directives
-airTrafficControlApp.directive('aircraftDetails', function(){
-   return{
-      templateUrl:'directives/aircraftDetails.html',
-      scope:{
-         aircraftObject: "="
-         // Restrict scope to only access
-         // aircraft object
-         // This directive will render the aircraft
-         // details. In the future, if there was a
-         // different way to render the aircraft details,
-         // only this directive will have to be changed 
-      }
-   } 
-});
